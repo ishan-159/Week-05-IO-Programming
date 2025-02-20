a17285960d69cb422c9fc556704914a2e3df0d1a
ishan-159 <ishantiwari159@gmail.com>
2025-02-20 16:22:26 +0530

    [Ishan] Add.Solution of read large csv file efficiently.

---
+++ Day1/src/main/java/com/csvdatahandling/advanceproblems/reallargecsv/LargeCSVReader.java
@@ -0,0 +1,33 @@
+package com.csvdatahandling.advanceproblems.reallargecsv;
+
+
+import java.io.*;
+
+public class LargeCSVReader {
+    public static void readCSVInChunks(String filePath, int chunkSize) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int count = 0;
+            int batchCount = 0;
+
+            while ((line = br.readLine()) != null) {
+                count++;
+
+                if (count % chunkSize == 0) {
+                    batchCount++;
+                    System.out.println("Processed " + count + " records so far (Batch: " + batchCount + ")");
+                }
+            }
+
+            System.out.println("Total records processed: " + count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\91887\\OneDrive\\Desktop\\Week-05-IO-Programming\\Day1\\src\\main\\java\\com\\csvdatahandling\\advanceproblems\\reallargecsv\\large_file.csv";
+        int chunkSize = 100;
+        readCSVInChunks(filePath, chunkSize);
+    }
+}
